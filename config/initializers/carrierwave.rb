57a2dc0225b352a2e944a8868edf15584d390426
Sam <sbrks3@gmail.com>
2016-07-07 12:18:51 -0700

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,1 @@
-CarrierWave.configure do |config|
-	config.fog_credentials = {
-		:provider => 'AWS',
-		:aws_access_key_id => 'AKIAIHC4CWW6EEPODLGA',
-		:aws_secret_access_key => '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',
-		:region => 'northern-california'
 
-	}
-
-	config.fog_directory = 'bucketlistbucket'
-	# config.fog_public = false
-end
