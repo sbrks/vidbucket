fb60ec9cb7811a3356452b537ac9d9b870ffc553
Sam <sbrks3@gmail.com>
2016-07-07 12:19:31 -0700

    Update videouploader.rb

--- models/videouploader.rb
+++ models/videouploader.rb
@@ -2,14 +2,6 @@
 # require 'rmagick'
 
 class VideoUploader < CarrierWave::Uploader::Base
-	# include CarrierWave::Rmagick
-	# version :thumb do
-	# 	process :resize_to_fill => [200, 200]
-	# end
-
-	#include rmagick or minimagick support:
-	# include CarrierWave::Rmagick 
-	#include CarrierWave::MiniMagick
 
 
 	#what kind of storage to use for this uploader
